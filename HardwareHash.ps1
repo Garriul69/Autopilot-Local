bcac5054442b15604faa729a74805b571b72414f
Gibran Lopez <80367200+Garriul69@users.noreply.github.com>
2026-02-07 09:37:25 -0600

    Update HardwareHash con NuGet

--- HardwareHash.ps1
+++ HardwareHash.ps1
@@ -1,12 +1,27 @@
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
+Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass -Force
+
+# Forzar NuGet
+if (-not (Get-PackageProvider -Name NuGet -ErrorAction SilentlyContinue)) {
+    Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force -Confirm:$false
+}
+
+# Confiar PSGallery
+Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
+
+# Asegurar PowerShellGet
+if (-not (Get-Module -ListAvailable PowerShellGet)) {
+    Install-Module PowerShellGet -Force -Confirm:$false
+}
+
+$env:Path += ";C:\Program Files\WindowsPowerShell\Scripts"
+
+# Instalar script Autopilot
+Install-Script -Name Get-WindowsAutopilotInfo -Force -Confirm:$false
+
 $ComputerName = $env:COMPUTERNAME
 $NetworkPath = "\\10.10.10.3\Local Intune\Hashes"
 $OutputFile = Join-Path $NetworkPath "$ComputerName.csv"
 
-$ScriptUrl = "https://raw.githubusercontent.com/microsoft/Intune-PowerShell-Scripts/master/WindowsAutopilot/Get-WindowsAutopilotInfo.ps1"
-$LocalScript = "$env:TEMP\Get-WindowsAutopilotInfo.ps1"
-
-Invoke-WebRequest -Uri $ScriptUrl -OutFile $LocalScript -UseBasicParsing
-
-& powershell.exe -ExecutionPolicy Bypass -File $LocalScript -OutputFile $OutputFile
+Get-WindowsAutopilotInfo -OutputFile $OutputFile
