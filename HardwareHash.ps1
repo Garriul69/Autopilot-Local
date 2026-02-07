281d78ab9ba6879e4fa1969d8484343ac8a26cb1
Gibran Lopez <80367200+Garriul69@users.noreply.github.com>
2026-02-07 09:25:27 -0600

    Get Hardware Hash Nuevos

--- HardwareHash.ps1
+++ HardwareHash.ps1
@@ -1,23 +1,15 @@
-# Forzar TLS 1.2 (requerido para Install-Script)
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
-# Permitir ejecución solo para esta sesión
-Set-ExecutionPolicy -Scope Process -ExecutionPolicy RemoteSigned -Force
+Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass -Force
 
-# Asegurar que el path de scripts esté disponible
+Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
+
 $env:Path += ";C:\Program Files\WindowsPowerShell\Scripts"
 
-# Instalar el script de Microsoft (solo la primera vez)
 Install-Script -Name Get-WindowsAutopilotInfo -Force
 
-# Obtener nombre del equipo
 $ComputerName = $env:COMPUTERNAME
-
-# Ruta del recurso compartido
 $NetworkPath = "\\10.10.10.3\Local Intune\Hashes"
-
-# Nombre final del archivo
 $OutputFile = Join-Path $NetworkPath "$ComputerName.csv"
 
-# Ejecutar el script y generar el CSV
 Get-WindowsAutopilotInfo -OutputFile $OutputFile
