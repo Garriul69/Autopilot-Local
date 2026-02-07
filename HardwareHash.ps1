56a9be01734ab6bb64eb3580fe2795adcf284e60
Gibran Lopez <80367200+Garriul69@users.noreply.github.com>
2026-02-07 09:32:56 -0600

    Hardware Hash
    Sin NuGet

--- HardwareHash.ps1
+++ HardwareHash.ps1
@@ -1,15 +1,12 @@
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
-Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass -Force
-
-Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
-
-$env:Path += ";C:\Program Files\WindowsPowerShell\Scripts"
-
-Install-Script -Name Get-WindowsAutopilotInfo -Force
-
 $ComputerName = $env:COMPUTERNAME
 $NetworkPath = "\\10.10.10.3\Local Intune\Hashes"
 $OutputFile = Join-Path $NetworkPath "$ComputerName.csv"
 
-Get-WindowsAutopilotInfo -OutputFile $OutputFile
+$ScriptUrl = "https://raw.githubusercontent.com/microsoft/Intune-PowerShell-Scripts/master/WindowsAutopilot/Get-WindowsAutopilotInfo.ps1"
+$LocalScript = "$env:TEMP\Get-WindowsAutopilotInfo.ps1"
+
+Invoke-WebRequest -Uri $ScriptUrl -OutFile $LocalScript -UseBasicParsing
+
+& powershell.exe -ExecutionPolicy Bypass -File $LocalScript -OutputFile $OutputFile
